d5590b838e8c7e770bb0ea3a12ad343c269913b9
Daniel Sissa <danielsissa@Daniels-MacBook-Pro.local>
2017-05-30 21:14:56 -0500

    touch sensituvity 35 to 55

--- js/main.js
+++ js/main.js
@@ -194,7 +194,7 @@ $(document).ready(function () {
         //		responsive: 900,
         scrollOverflow: true,
         autoScrolling: true,
-        touchSensitivity: 35,
+        touchSensitivity: 55,
         bigSectionsDestination: 'top',
         css3: false,
         navigation: true,
